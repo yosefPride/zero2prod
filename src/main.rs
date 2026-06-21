20472340da832a559dedca6991ca33be8e59b368
yosef <yosefchaimpride@gmail.com>
2026-06-21 15:29:17 +0300

    Added listener for binding.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-use zero2prod::run;
+use std::net::TcpListener;
+
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    // Bubble up the io::Error if we failed to bind the address
-    // Otherwise call .await on our Server
-    run()?.await
+async fn main() -> std::io::Result<()> {
+    // 1. Bind to your desired production address and port
+    let address = "127.0.0.1:8080";
+    let listener = TcpListener::bind(address).expect("Failed to bind to port 8080");
+
+    println!("Listening on http://{}", address);
+
+    // 2. Pass the listener to your architectural run function
+    // and .await it so the main function doesn't exit immediately.
+    zero2prod::run(listener)?.await
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,11 +1,22 @@
+use std::net::TcpListener;
+
 // if we fail to perform the required setup we can just panic and crash
 // all the things.
-fn spawn_app() {
-    let server = zero2prod::run().expect("Failed to bind address");
+fn spawn_app() -> String {
+    // Port 0 is special-cased at the OS level: trying to bind port 0 will trigger an OS scan for an available port which
+    // will then be bound to the application.
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
+
+    // We retrieve the port assigned to us by the OS
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind address");
+
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future,
     // but we have no use for it here, hence the non-binding let
     let _ = tokio::spawn(server);
+    // We return the application address to the caller!
+    return format!("http://127.0.0.1:{}", port);
 }
 
 // `tokio::test` is the testing equivalent of `tokio::main`.
@@ -16,14 +27,14 @@ fn spawn_app() {
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    spawn_app();
+    let address = spawn_app();
     // We need to bring in `reqwest`
     // to perform HTTP requests against our application.
     let client = reqwest::Client::new();
 
     // Act
     let response = client
-        .get("http://127.0.0.1:8000/health_check")
+        .get(&format!("{}/health_check", &address))
         .send()
         .await
         .expect("Failed to execute request.");
