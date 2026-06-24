36fa52f536e03c1d91fcad7824022e76d9cc9a36
yosef <yosefchaimpride@gmail.com>
2026-06-24 16:38:26 +0300

    changed file setup

--- src/lib.rs
+++ src/lib.rs
@@ -1,31 +1,7 @@
-use actix_web::dev::Server;
-use actix_web::{App, HttpResponse, HttpServer, web};
-use std::net::TcpListener;
+// use actix_web::dev::Server;
+// use actix_web::{App, HttpResponse, HttpServer, web};
+// use std::net::TcpListener;
 
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-// Let's start simple: we always return a 200 OK
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    //TcpListener exposes the open port we ended up binding using port 0.
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            // A new entry in our routing table for POST /subscriptions requests
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
---
+++ src/startup.rs
@@ -0,0 +1,16 @@
+use actix_web::dev::Server;
+use actix_web::{App, HttpServer, web};
+use std::net::TcpListener;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    //TcpListener exposes the open port we ended up binding using port 0.
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            // A new entry in our routing table for POST /subscriptions requests
+            .route("/subscriptions", web::post().to(subscribe))
+    })
+    .listen(listener)?
+    .run();
+    Ok(server)
+}
